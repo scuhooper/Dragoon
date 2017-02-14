c298d50b92f659da5ff650e53df699a9c6c91b58
scuhooper <keeling18@marshall.edu>
2017-02-14 03:00:59 -0500

    DragoonAIController
    Created an AI controller class to be customized to be used to control
    enemy agents.

---
+++ Source/Dragoon/DragoonAIController.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Dragoon.h"
+#include "DragoonAIController.h"
+
+ADragoonAIController::ADragoonAIController() {
+	// do super awesome AI stuff
+}
---
+++ Source/Dragoon/DragoonAIController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "DragoonAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAGOON_API ADragoonAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	ADragoonAIController();
+};
