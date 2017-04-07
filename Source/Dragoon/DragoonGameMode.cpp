ef71907f1bae911de8e9512ea608bfae1ee35553
scuhooper <keeling18@marshall.edu>
2017-04-07 17:21:18 -0400

    Levels, Character BPs, Game Mode
    Levels: Added player and enemy agents to Level 1 The Hub.
    Levels: Added textures to floor of Level 1 The Hub.
    Levels: Added lights to layout of Level 1 The Hub.
    Character BPs: Set default values for damage and strong attack
    multiplier for both DragoonCharacter and EnemyAgent blueprints.
    Game Mode: Changed default Pawn class of DragoonGameMode to be
    DragoonCharacter_BP.

--- Source/Dragoon/DragoonGameMode.cpp
+++ Source/Dragoon/DragoonGameMode.cpp
@@ -7,7 +7,6 @@
 ADragoonGameMode::ADragoonGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
